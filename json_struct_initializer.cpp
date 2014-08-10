7b7a3f29caaf064f16ffef3371cc212d5fcb8bb5
iliar1987 <iliaraysin@gmail.com>
2014-08-10 22:18:14 +0300

    just found to code files not included in repository

---
+++ json_struct_initializer.cpp
@@ -0,0 +1,69 @@
+#include "json_struct_initializer.h"
+
+//namespace rj { using namespace rapidjson; }
+//
+//using namespace rj;
+
+void rj::GetJsonValue(bool& param, rj::Value &obj)
+{
+	if (!obj.IsBool())
+		throw(std::runtime_error("variable is not bool"));
+	param = obj.GetBool();
+}
+
+void rj::GetJsonValue(int& param, rj::Value &obj)
+{
+	if (!obj.IsInt())
+		throw(std::runtime_error("variable is not int"));
+	param = obj.GetInt();
+}
+
+void rj::GetJsonValue(unsigned char & param, rj::Value &obj)
+{
+	if (!obj.IsInt())
+		throw(std::runtime_error("variable is not int"));
+	param = obj.GetInt();
+}
+
+void rj::GetJsonValue(float& param, rj::Value &obj)
+{
+	if (obj.IsDouble())
+		param = static_cast<float>(obj.GetDouble());
+	else if (obj.IsInt())
+		param = static_cast<float>(obj.GetInt());
+	else
+		throw(std::runtime_error("variable is of wrong type"));
+}
+
+void rj::GetJsonValue(double& param, rj::Value &obj)
+{
+	if (obj.IsDouble())
+		param = obj.GetDouble();
+	else if (obj.IsInt())
+		param = static_cast<double>(obj.GetInt());
+	else
+		throw(std::runtime_error("variable is of wrong type"));
+}
+
+template<typename T> void rj::GetJsonValue(rj::OptionallyLogarithmic<T> &param, rj::Value &obj)
+{
+	param.FromRapidJsonObject(reinterpret_cast<rapidjson::Value&>(obj));
+}
+
+
+template<typename T> void rj::OptionallyLogarithmic<T>::FromRapidJsonObject(rj::Value &obj)
+{
+	if (obj.HasMember("db"))
+		SetDB(obj["db"].GetDouble());
+	else if (obj.HasMember("linear"))
+		SetLinear(obj["linear"].GetDouble());
+	else
+		throw(std::runtime_error("variable should have 'db' or 'linear' member in it."));
+}
+
+typedef rj::OptionallyLogarithmic<float> dummy3;
+void dummy_func4()
+{
+	dummy3 someval;
+	rj::GetJsonValue(someval, rapidjson::Value());
+}
---
+++ json_struct_initializer.h
@@ -0,0 +1,92 @@
+#include "boost\preprocessor\repetition\repeat.hpp"
+#include "boost\preprocessor\seq\for_each_i.hpp"
+#include "boost\preprocessor\tuple\elem.hpp"
+#include "boost\preprocessor\stringize.hpp"
+#include "rapidjson\filestream.h"
+#include "rapidjson\document.h"
+
+// example list of typevar tuples:
+// #define MY_FIELDS ((bool,x))((int,y))
+
+#define DEF_VAR(r,suffix,i,elem) BOOST_PP_TUPLE_ELEM(2,0,elem) BOOST_PP_TUPLE_ELEM(2,1,elem) suffix
+
+#define DEF_ALL_VARS(tuples_list) \
+	BOOST_PP_SEQ_FOR_EACH_I(DEF_VAR, ; , tuples_list)
+
+#include <exception>
+
+//class JsonInitException : public std::runtime_error
+//{
+//public:
+//	JsonInitException(char* str)
+//		: runtime_error(str)
+//	{}
+//	JsonInit
+//};
+
+#define EXTRACT_FROM_JSON_OBJ(t,a,obj_name) \
+	try{\
+	GetJsonValue((a), obj_name[BOOST_PP_STRINGIZE(a)]); \
+	}\
+	catch (exception& re) \
+		{ std::cerr << re.what() << std::endl; \
+		throw runtime_error("error initializing parameter: " BOOST_PP_STRINGIZE(a)); }\
+	catch (...) { throw runtime_error("Unknown error parsing parameter: " BOOST_PP_STRINGIZE(a)); }
+
+#define EXTRACT_FROM_JSON_OBJ_typevar_tuple(r, obj_name, i, typevar_tuple) \
+	EXTRACT_FROM_JSON_OBJ(BOOST_PP_TUPLE_ELEM(2, 0, typevar_tuple)\
+	, BOOST_PP_TUPLE_ELEM(2, 1, typevar_tuple)\
+	, obj_name)
+
+#define OUTPUT_FIELD(FIELD) \
+	<< " " BOOST_PP_STRINGIZE(FIELD) " " \
+	<< FIELD
+
+#define OUTPUT_FIELD_typevar_tuple(r,d,i,typevar_tuple) \
+	OUTPUT_FIELD(BOOST_PP_TUPLE_ELEM(2, 1, typevar_tuple))
+
+#define OUTPUT_ALL(seq_of_typevar_tuples) \
+	BOOST_PP_SEQ_FOR_EACH_I(OUTPUT_FIELD_typevar_tuple, 0\
+	, seq_of_typevar_tuples)
+
+#define INITIALIZE_FIELDS_FROM_RAPIDJSON_OBJ(list_of_tuples,obj) \
+	BOOST_PP_SEQ_FOR_EACH_I(\
+	EXTRACT_FROM_JSON_OBJ_typevar_tuple\
+	, obj\
+	, list_of_tuples)
+namespace rj {
+
+	using namespace rapidjson;
+
+	template <typename T> inline T db_to_lin(T value)
+	{
+		return std::pow(10.0, value / 10.0);
+	}
+	template <typename T> inline T lin_to_db(T value)
+	{
+		return 10.0*std::log10(value);
+	}
+
+	template <typename T> class OptionallyLogarithmic
+	{
+		T value;
+	public:
+		operator T() { return GetLinear(); }
+		OptionallyLogarithmic(T _value) { value = _value; }
+		OptionallyLogarithmic() {}
+		T GetLinear() { return value; }
+		T GetDB() { return 10.0*std::log10(value); }
+		void SetLinear(T _value) { value = _value; }
+		void SetDB(T _value) { value = db_to_lin(_value); }
+		void FromRapidJsonObject(rapidjson::Value &obj);
+		OptionallyLogarithmic(rapidjson::Value &obj) { FromRapidJsonObject(obj); }
+	};
+
+
+	void GetJsonValue(bool& param, rj::Value &obj);
+	void GetJsonValue(int& param, rj::Value &obj);
+	void GetJsonValue(unsigned char & param, rj::Value &obj);
+	void GetJsonValue(float& param, rj::Value &obj);
+	void GetJsonValue(double& param, rj::Value &obj);
+	template<typename T> void GetJsonValue(OptionallyLogarithmic<T> &param, rj::Value &obj);
+}
